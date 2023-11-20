510982331581ccae878b236b49b0f32f6991d289
raddacle <50691489+raddacle@users.noreply.github.com>
2023-11-20 04:03:55 -0800

    Update ubuntu-vm.sh

--- vm/ubuntu-vm.sh
+++ vm/ubuntu-vm.sh
@@ -407,7 +407,7 @@ pvesm alloc $STORAGE $VMID $DISK0 4M 1>&/dev/null
 qm importdisk $VMID ${FILE} $STORAGE ${DISK_IMPORT:-} 1>&/dev/null
 qm set $VMID \
   -efidisk0 ${DISK0_REF}${FORMAT} \
-  -scsi0 ${DISK1_REF},${DISK_CACHE}${THIN}size=2G \
+  -scsi0 ${DISK1_REF},${DISK_CACHE}${THIN}size=15G \
   -ide2 local-lvm:cloudinit \
   -boot order=scsi0 \
   -serial0 socket \
